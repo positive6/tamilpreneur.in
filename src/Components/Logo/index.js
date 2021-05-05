2660fbcc4bd6489d5cdadd338963f744e09adb38
positive6 <shriparvathi95@gmail.com>
2021-05-05 16:57:13 +0530

    added Carousel

--- src/Components/Logo/index.js
+++ src/Components/Logo/index.js
@@ -1,14 +1,39 @@
 import { Container } from './style';
-import logo from '../../img/logo.jpeg';
+import carousel1 from '../../img/Carousel1.png';
+import carousel2 from '../../img/Carousel2.png';
+import carousel3 from '../../img/Carousel3.png';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Logo = () => {
-    return(
+    return (
         <Container>
-            <img src={logo}/>
-            <div className="content">
-                <h1>WHERE PASSION MEETS EDUCATION</h1> 
-                <h3>Building a community of full stack entrepreneurs!</h3>
-            </div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={carousel1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h4>Join Tamilpreneur Club <a href="club.tamilpreneur.in">here.</a></h4>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={carousel2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src= {carousel3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+            
         </Container>
     )
 }
--- src/Components/Logo/style.js
+++ src/Components/Logo/style.js
@@ -2,15 +2,23 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
+    height: 30%;
     background: black;
-    h1 {
-        font-size: 33px;
-        color: rgb(247, 247, 247);
-    }
-    h3 {
-        font-size: 22px;
-        color: rgb(169, 169, 169);
-    }
+    
+    .carousel-caption {
+        h3 {
+            font-size: 28px;
+            color: rgb(247, 247, 247);           
+        }
+        p {
+            font-size: 20px;         
+        }
+       padding-left:27%;
+       top: 65%;
+       bottom: 10%;
+       font-family: 'BebasNeueRegular', sans-serif;
+        
+      }
     @media only screen and (min-width: 1024px) {
         .content {
             padding: 13%;
